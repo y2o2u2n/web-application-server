c67f48b364e49c76d023e77a489f96fb0f58cb77
y2o2u2n <y2o2u2n@gmail.com>
2021-07-26 22:20:36 +0900

    feat : 4.2.1.6

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -8,7 +8,9 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.Collection;
 import java.util.Map;
 
 import db.DataBase;
@@ -42,12 +44,15 @@ public class RequestHandler extends Thread {
             }
 
             String[] tokens = line.split(" ");
+            boolean logined = false;
             int contentLength = 0;
             while (!"".equals(line)) {
                 log.debug("header : {}", line);
                 line = br.readLine();
                 if (line.contains("Content-Length")) {
                     contentLength = getContentLength(line);
+                } else if (line.contains("Cookie")) {
+                    logined = isLogin(line);
                 }
             }
 
@@ -56,10 +61,10 @@ public class RequestHandler extends Thread {
                 String body = IOUtils.readData(br, contentLength);
                 Map<String, String> params = HttpRequestUtils.parseQueryString(body);
                 User user = new User(
-                    params.get("userId"),
-                    params.get("password"),
-                    params.get("name"),
-                    params.get("email")
+                        params.get("userId"),
+                        params.get("password"),
+                        params.get("name"),
+                        params.get("email")
                 );
                 log.debug("User : {}", user);
                 DataBase.addUser(user);
@@ -80,6 +85,26 @@ public class RequestHandler extends Thread {
                 } else {
                     responseResource(out, "/user/login_failed.html");
                 }
+            } else if ("/user/list".equals(url)) {
+                if (!logined) {
+                    responseResource(out, "/user/login.html");
+                }
+
+                Collection<User> users = DataBase.findAll();
+                StringBuilder sb = new StringBuilder();
+                sb.append("<table border='1'>");
+                for (User user : users) {
+                    sb.append("<tr>");
+                    sb.append("<td>" + user.getUserId() + "</td>");
+                    sb.append("<td>" + user.getName() + "</td>");
+                    sb.append("<td>" + user.getEmail() + "</td>");
+                    sb.append("</tr>");
+                }
+                sb.append("</table>");
+                byte[] body = sb.toString().getBytes();
+                DataOutputStream dos = new DataOutputStream(out);
+                response200Header(dos, body.length);
+                responseBody(dos, body);
             } else {
                 responseResource(out, url);
             }
@@ -91,6 +116,16 @@ public class RequestHandler extends Thread {
     private int getContentLength(String line) {
         String[] headerTokens = line.split(":");
         return Integer.parseInt(headerTokens[1].trim());
+    }
+
+    private boolean isLogin(String line) {
+        String[] headerTokens = line.split(":");
+        Map<String, String> cookies = HttpRequestUtils.parseCookies(headerTokens[1].trim());
+        String value = cookies.get("logined");
+        if (value == null) {
+            return false;
+        }
+        return Boolean.parseBoolean(value);
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
